bc372f881d75f21f97e1d7ecadc673f055479c12
hideyorimurakami <71474250+hideyorimurakami@users.noreply.github.com>
2020-10-27 10:14:48 +0900

    Add files via upload

--- hideyorimurakami/src/main/java/com/example/demo/models/Jankenuser.java
+++ hideyorimurakami/src/main/java/com/example/demo/models/Jankenuser.java
@@ -2,24 +2,19 @@ package com.example.demo.models;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
-
-import org.springframework.context.annotation.Scope;
 
 import lombok.Data;
 
 @Data
 @Entity
-@Scope("session")
 public class Jankenuser {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private int id;
 
-	@Column(name = "sessionId")
+	@Column(name = "usrId" )
+	private int usrId;
+
+	@Id
 	private String sessionId;
 
 	@Column(name = "round")
