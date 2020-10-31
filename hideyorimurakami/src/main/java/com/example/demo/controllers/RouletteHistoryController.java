c9407c7d38842bdf36a5dec641c232db8aeab6fb
hideyorimurakami <71474250+hideyorimurakami@users.noreply.github.com>
2020-10-31 11:01:03 +0900

    Add files via upload

--- hideyorimurakami/src/main/java/com/example/demo/controllers/RouletteHistoryController.java
+++ hideyorimurakami/src/main/java/com/example/demo/controllers/RouletteHistoryController.java
@@ -11,14 +11,14 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.example.demo.models.Rireki;
+import com.example.demo.models.RouletteHistory;
 
 @Controller
-public class RouletterirekiController {
-	ArrayList<Rireki> al ;
-	ArrayList<Rireki> al2 ;
-	ArrayList<Rireki> al3 ;
-@GetMapping("/rouletterireki")
+public class RouletteHistoryController {
+	ArrayList<RouletteHistory> al ;
+	ArrayList<RouletteHistory> al2 ;
+	ArrayList<RouletteHistory> al3 ;
+@GetMapping("/roulettehistoryboard")
 public String getRouletterirek(HttpSession s,Model model) {
 	al = new ArrayList<>();
 	al2 = new ArrayList<>();
@@ -29,33 +29,33 @@ public String getRouletterirek(HttpSession s,Model model) {
 	s.setAttribute("s", al);
 	s.setAttribute("s2", al2);
 	s.setAttribute("s3", al3);
-	return "roulette/rireki";
+	return "roulette/roulettehistory";
 }
-@PostMapping("/rouletterireki")
-public String rouletterirek(@RequestParam int num, @ModelAttribute Rireki rireki ,Model model,HttpSession s) {
+@PostMapping("/roulettehistoryboard")
+public String rouletterirek(@RequestParam int num, @ModelAttribute RouletteHistory roulettehistory ,Model model,HttpSession s) {
 	s.getAttribute("s");
-	al = (ArrayList<Rireki>)s.getAttribute("s");
-	al2 = (ArrayList<Rireki>)s.getAttribute("s2");
-	al3 = (ArrayList<Rireki>)s.getAttribute("s3");
+	al = (ArrayList<RouletteHistory>)s.getAttribute("s");
+	al2 = (ArrayList<RouletteHistory>)s.getAttribute("s2");
+	al3 = (ArrayList<RouletteHistory>)s.getAttribute("s3");
 	if(num==0) {
 		al.clear();
 		al2.clear();
 		al3.clear();
 	}else {
-		rireki.setNum(num);
+		roulettehistory.setNum(num);
 
 		if(al.size() < 13) {
-			al.add(0,rireki);
+			al.add(0,roulettehistory);
 		}else if(al2.size() < 13) {
 			al2.add(0, al.get(al.size() -1));
 			al.remove(al.size() -1);
-			al.add(0,rireki);
+			al.add(0,roulettehistory);
 		}else {
 			al3.add(0, al2.get(al.size() -1));
 			al2.remove(al2.size() -1);
 			al2.add(0, al.get(al.size() -1));
 			al.remove(al.size() -1);
-			al.add(0,rireki);
+			al.add(0,roulettehistory);
 			if(al3.size() ==14) {
 				al3.remove(al3.size() -1);
 			}
@@ -71,6 +71,6 @@ public String rouletterirek(@RequestParam int num, @ModelAttribute Rireki rireki
 	s.setAttribute("s", al);
 	s.setAttribute("s2", al2);
 	s.setAttribute("s3", al3);
-	return "roulette/rireki";
+	return "roulette/roulettehistory";
 }
 }
