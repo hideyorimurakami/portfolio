a105f3c39623497fcb2cb93824c8b8827659ed29
hideyorimurakami <71474250+hideyorimurakami@users.noreply.github.com>
2020-11-04 13:47:01 +0900

    Add files via upload

--- hideyorimurakami/src/main/java/com/example/demo/configs/WebSecurityConfig.java
+++ hideyorimurakami/src/main/java/com/example/demo/configs/WebSecurityConfig.java
@@ -1,9 +1,15 @@
 package com.example.demo.configs;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import lombok.RequiredArgsConstructor;
 
@@ -11,43 +17,53 @@ import lombok.RequiredArgsConstructor;
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-//    private final UserDetailsService userDetailsService;
+	private final UserDetailsService userDetailsService;
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-            .antMatchers("/**").permitAll();
-        http.csrf().disable();
-        http.headers().frameOptions().disable();
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
 
-        // 認証サンプル動作時は、以下のコメントを外してください
-        /*
-        http.authorizeRequests()
-            .antMatchers("/login").permitAll()
-            .anyRequest().authenticated()
-            .and()
-            .formLogin()
-            .loginPage("/login")
-            .usernameParameter("usr")
-            .passwordParameter("passwd")
-            .and()
-            .logout()
-            .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-            .logoutSuccessUrl("/admin/index")
-             .invalidateHttpSession(true);
-      */
-    }
 
-//    @Override
-//    protected void configure(AuthenticationManagerBuilder auth)
-//        throws Exception {
-//        auth
-//            .userDetailsService(userDetailsService)
-//            .passwordEncoder(passwordEncoder());
-//    }
-//
-//    @Bean
-//    public PasswordEncoder passwordEncoder() {
-//        return new BCryptPasswordEncoder();
-//    }
+		http.csrf().disable();
+		http.headers().frameOptions().disable();
+
+
+		http.authorizeRequests()
+		// ログイン不要ページの設定
+		.antMatchers("/h2-console/**").permitAll()
+				.antMatchers("/webjars/**").permitAll() //webjarsへアクセス許可
+				.antMatchers("/css/**").permitAll() //cssへアクセス許可
+				.antMatchers("/img/**").permitAll()
+				.antMatchers("/login").permitAll() //ログインページは直リンクOK
+				.antMatchers("/signup").permitAll() //ユーザー登録画面は直リンクOK
+				.antMatchers("/").permitAll()
+				.antMatchers("/blackjackstrategy").permitAll()
+				.antMatchers("/janken").permitAll()
+				.antMatchers("/roulettecalculator").permitAll()
+				.antMatchers("/roulettehistoryboard").permitAll()
+				.anyRequest().authenticated()
+				.and()
+				.formLogin()
+				.loginPage("/login")
+				.usernameParameter("userId")
+				.passwordParameter("password")
+				.and()
+				.logout()
+				.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+				.logoutSuccessUrl("/admin/index")
+				.invalidateHttpSession(true);
+
+	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth)
+			throws Exception {
+		auth
+				.userDetailsService(userDetailsService)
+				.passwordEncoder(passwordEncoder());
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 }
