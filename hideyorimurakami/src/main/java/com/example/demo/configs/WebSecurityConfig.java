99bdcac4805847605ed7d876d8e2d5b1dcbb7cbd
hideyorimurakami <71474250+hideyorimurakami@users.noreply.github.com>
2020-11-04 22:49:48 +0900

    Add files via upload

--- hideyorimurakami/src/main/java/com/example/demo/configs/WebSecurityConfig.java
+++ hideyorimurakami/src/main/java/com/example/demo/configs/WebSecurityConfig.java
@@ -23,8 +23,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 
 
-		http.csrf().disable();
-		http.headers().frameOptions().disable();
+//		http.csrf().disable();
+//		http.headers().frameOptions().disable();
 
 
 		http.authorizeRequests()
